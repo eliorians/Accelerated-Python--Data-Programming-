721d5be7698ae9736bcedd2f0c06f875062a3db8
Eli Orians <eli.orians@gmail.com>
2022-08-31 13:48:22 -0400

    done

--- cs01/lorentz.py
+++ cs01/lorentz.py
@@ -1,8 +1,6 @@
-from cmath import sqrt
-
 
 c = 3e8
 s = float(input("Enter the velocity (m/s): "))
-result = 1/ sqrt(1 - (s**2 / c**2))
+result = 1/ ((1 - (s**2 / c**2))**(1/2))
 
 print(f'The Lorentz factor at {s} is {result:.4f}.')
