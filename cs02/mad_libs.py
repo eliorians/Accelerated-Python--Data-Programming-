9690c817343a3961f883951d980c7b184ce8cb09
Eli Orians <eli.orians@gmail.com>
2022-09-05 21:51:29 -0400

    first 2 problems done at desk

---
+++ cs02/mad_libs.py
@@ -0,0 +1,9 @@
+name_a = input("Enter a name: ")
+name_b = input("Enter another name: ")
+number_a = int(input("Enter an integer between 10-30: "))
+number_b = int(input("Enter another integer between 10-30: "))
+thing = input("Enter a thing: ")
+
+print(f'{name_a} had {number_a} {thing}.\n')
+print(f'{name_b} had {number_b} {thing}.\n')
+print(f'How many {thing} did they have in all?\n')
--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-a = 0 
-while a < 100: 
-    print('*', end='') 
-print()
+x = 5 
+while x > 0: 
+    y  = int(input()) 
+    if y == 25:
+        None 
+    else:
+        x -= 1 
+        print(f'x = {x}')
