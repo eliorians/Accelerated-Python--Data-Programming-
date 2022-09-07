6c38a88f3a28f54f70b503c5aaaaf5f282f22fb3
Eli Orians <eli.orians@gmail.com>
2022-09-07 09:16:39 -0400

    coin flips done

--- cs02/coin_flips.py
+++ cs02/coin_flips.py
@@ -1,9 +1,15 @@
-import coin_flip_game
-
-count = 0
+from random import randint
 wins = 0
 
-while (count < 10):
-    if (coin_flip_game):
+for a in range(10):
+    
+    guess = int(input("What is your guess (1 or 2): "))
+    coin = randint(1,2)
+
+    if guess == coin:
+        print(f'You are correct {guess} == {coin}.')
         wins += 1
-    count += 1
+    else:
+        print(f'Sorry, {guess} != {coin}.')
+
+print(f'You were correct {wins} times.')
