3ed69bf713dcdc520d32e93353db1ae564264802
Eli Orians <eli.orians@gmail.com>
2022-09-12 12:12:40 -0400

    need to continue on linux-desk

--- cs03/audio.py
+++ cs03/audio.py
@@ -2,9 +2,9 @@ from audio_helpers import load, save
 import matplotlib.pyplot as plt
 
 #get filename
-filename = input("Enter the WAV file name: ")
+file_name = input("Enter the WAV file name: ")
 #retrieve audio
-rate, data = load(filename)
+rate, data = load(file_name)
 #save audio
 data[0] = 0
 save('edited.wav', rate, data)
@@ -14,9 +14,9 @@ y = [data]
 plt.plot(x,y)
 plt.xlabel('Time (seconds)')
 plt.ylabel('Sound Pressure')
-plt.title(filename)
+plt.title(file_name)
 plt.show
 
 print(f'There are {data.len()} samples.')
 print(f'The sample rate is {data.len() / rate.len()} samples/sec.')
-print(f'The file is {rate.len()} seconds long.')
+print(f'The file is {rate.len():.3f} seconds long.')
