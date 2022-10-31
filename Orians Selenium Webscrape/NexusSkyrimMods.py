4e85af29248b2f7d2d1d110b9c9f04a5ec6c819c
Eli Orians <eli.orians@gmail.com>
2022-10-31 10:24:04 -0400

    up and running

--- Orians Selenium Webscrape/NexusSkyrimMods.py
+++ Orians Selenium Webscrape/NexusSkyrimMods.py
@@ -5,19 +5,25 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path='C:\\Users\\Eli Orians\\chromedriver_win32\\chromedriver.exe')
 driver.get('https://www.nexusmods.com/skyrim/mods/')
-assert "NexusSkyrimMods" in driver.title
 
 
 #Attributes
 modName = []            #name of mod
-modLink = []            #link to the mod
-modAuthor = []          #original writer of mod
-modUploader = []        #uploader of mod
-modUploaderProfile = [] #link to uploader profiler
-modDesc = []            #mod description
-modSize = []            #download size of mod
-modLikes = []           #number of likes
-modDate = []            #original upload date 
-modUpdate = []          #date of last update
+#modLink = []            #link to the mod
+#modAuthor = []          #original writer of mod
+#modUploader = []        #uploader of mod
+#modUploaderProfile = [] #link to uploader profiler
+#modDesc = []            #mod description
+#modSize = []            #download size of mod
+#modLikes = []           #number of likes
+#modDate = []            #original upload date 
+#modUpdate = []          #date of last update
+
+
+for i in driver.find_elements(By.XPATH, r'//li[@class="mod-tile"]//p[@class="tile-name"]'):
+    modName.append(i.text)
+modName = modName[::2]
+
+driver.quit()
