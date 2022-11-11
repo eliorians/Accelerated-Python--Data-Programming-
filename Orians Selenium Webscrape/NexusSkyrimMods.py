c62607085f3ad6ed6d3806319ece4344dbdabf6a
Eli Orians <eli.orians@gmail.com>
2022-11-11 09:08:52 -0500

    cs07

--- Orians Selenium Webscrape/NexusSkyrimMods.py
+++ Orians Selenium Webscrape/NexusSkyrimMods.py
@@ -1,29 +1,104 @@
 
-#Eli Orians
+#! Eli Orians
+
+
+#! Resources
+#https://selenium-python.readthedocs.io/
+
+
+
+#! Additional Work
+#8 Attributes with an equal mix of css and xpath
+#Scraped 100 pages
+#String and List manipulation to make data content presentable
+#used selenium to click to next page
+#used sleep to wait for webpage to load
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import json
+import time
 
 driver = webdriver.Chrome(executable_path='C:\\Users\\Eli Orians\\chromedriver_win32\\chromedriver.exe')
-driver.get('https://www.nexusmods.com/skyrim/mods/')
-
+url = 'https://www.nexusmods.com/skyrim/mods/'
 
 #Attributes
-modName = []            #name of mod
-#modLink = []            #link to the mod
-#modAuthor = []          #original writer of mod
-#modUploader = []        #uploader of mod
-#modUploaderProfile = [] #link to uploader profiler
-#modDesc = []            #mod description
-#modSize = []            #download size of mod
-#modLikes = []           #number of likes
-#modDate = []            #original upload date 
-#modUpdate = []          #date of last update
+name = []            #name of mod
+desc = []            #mod description
+link = []            #link to the mod
+author = []          #original writer of mod
+uploader = []        #uploader of mod
+date = []            #original upload date
+update = []          #date of last update for the mod
+likes = []           #number of likes
 
 
-for i in driver.find_elements(By.XPATH, r'//li[@class="mod-tile"]//p[@class="tile-name"]'):
-    modName.append(i.text)
-modName = modName[::2]
+for i in range(100):
+
+    driver.get(url)
+    time.sleep(7)
+
+    #name
+    for i in driver.find_elements(By.XPATH, r'//li[@class="mod-tile"]//p[@class="tile-name"]'):
+        name.append(i.text)
+
+    #description
+    for i in driver.find_elements(By.CSS_SELECTOR, r'p.desc'):
+        desc.append(i.text)
+
+    #link
+    for i in driver.find_elements(By.XPATH, r'//li[@class="mod-tile"]//p[@class="tile-name"]//a'):
+        link.append(i.get_attribute('href'))
+
+    #author
+    for i in driver.find_elements(By.CSS_SELECTOR, r'div.realauthor'):
+        author.append(i.text)
+
+    #uploader
+    for i in driver.find_elements(By.CSS_SELECTOR, r'div.author'):
+        uploader.append(i.text)
+
+    #date
+    for i in driver.find_elements(By.XPATH, r'//li[@class="mod-tile"]//time'):
+        date.append(i.get_attribute('datetime'))
+
+    #update
+    for i in driver.find_elements(By.CSS_SELECTOR, r'div.date'):
+        update.append(i.text)  
+
+    #likes
+    for i in driver.find_elements(By.XPATH, r'//li[@class="mod-tile"]//li[@class="endorsecount inline-flex"]//span[@class="flex-label"]'):
+        likes.append(i.text)
+
+    #loop by .click() next page button and updating url
+    driver.find_element(By.XPATH, r'//li[@class="next"]/a').click()
+    url = driver.current_url
+
 
 driver.quit()
+
+#format content
+name = list(filter(None, name))
+link = list(filter(None, link))
+desc = list(filter(None, desc))
+date = list(filter(None, date))
+update = list(filter(None, update))
+uploader = list(filter(None, uploader))
+
+
+dic ={
+    "Mod Title": name,
+    "Mod Description": desc,
+    "Link to Mod": link,
+    "Original Author": author,
+    "Mod Uploader": uploader,
+    "Mod Endorsments": likes,
+    "Original Upload Date": date,
+    "Last Update": update
+    }
+
+with open('NexusSkyrimMods.json', 'a') as write_file:
+    json.dump(dic, write_file, indent = 4)
+
+
---
+++ cs07/cs07.py
@@ -0,0 +1,295 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+#1.1
+def construct_file_name(lat, lon):
+    filename = 'USGS_NED_1_'
+    
+    #lat formatting
+    if lat < 0:
+        filename = filename + 's' + str(lat*-1)
+    else:
+        filename = filename + 'n' + str(lat)
+
+    #lon formatting
+    if (abs(lon) < 100):
+        if lon < 0:
+            filename = filename + 'w0' + str(lon*-1)
+        else:
+            filename = filename + 'e0' + str(lon)
+    else:
+        if lon < 0:
+            filename = filename + 'w' + str(lon*-1)
+        else:
+            filename = filename + 'e' + str(lon)
+
+    filename = filename + '_IMG.tif'
+    return filename
+
+
+
+#1.2
+def load_trim_image(lat, lon):
+    im = plt.imread(construct_file_name(lat, lon))
+    im = im[6:-6, 6:-6]
+
+    return im
+
+
+#1.3
+def stitch_four(lat, lon):
+    im1 = load_trim_image(lat, lon)
+    im2 = load_trim_image(lat, lon+1)
+    im3 = load_trim_image(lat-1, lon)
+    im4 = load_trim_image(lat-1, lon+1)
+
+    t = np.concatenate((im1, im2), axis = 1)
+    b = np.concatenate((im3, im4), axis = 1)
+    im = np.concatenate((t, b), axis = 0)
+
+    return im
+
+
+#1.4
+def get_row(lat, lon_min, num_tiles):
+    
+    im = load_trim_image(lat, lon_min)
+    for i in range(1, num_tiles):
+        im = np.hstack((im, load_trim_image(lat, lon_min+i)))
+    
+    return im
+
+
+#1.5
+def get_tile_grid(lat_max, lon_min, num_lat, num_lon):
+    
+    im = get_row(lat_max, lon_min, num_lon)
+    for i in range(1, num_lat):
+        im = np.vstack((im, get_row(lat_max-i, lon_min, num_lon)))
+    
+    return im
+
+#1.6
+def get_northwest(lat, lon):
+
+    #lat rounding
+    if (not is_whole(lat)):
+        lat = int(str(lat).split('.')[0])
+        if (lat > 0):
+            lat = lat + 1
+        else:
+            lat = lat - 1
+    else:
+        lat = int(lat)
+
+    #lon rounding
+    if (not is_whole(lon)):
+        lon = int(str(lon).split('.')[0])
+        if (lon > 0):
+            lon = lon + 1
+        else:
+            lon = lon - 1
+    else:
+        lon = int(lon)
+
+    return (lat, lon)
+
+def is_whole(n):
+    return n % 1 == 0
+
+#1.7
+def get_tile_grid_decimal(northwest, southeast):
+
+    (la1, lo1) = northwest
+    (la2, lo2) = southeast
+    
+    lat1, lon1 = get_northwest(la1, lo1)
+    lat2, lon2 = get_northwest(la2, lo2)
+
+    num_lat = abs(lat2-lat1)
+    num_lon = abs(lon2-lon1)
+
+    return get_tile_grid(lat1, lon1, num_lat+1, num_lon+1)
+
+
+# -- Testing Section 1 -- #
+
+# print('1.1 testing')
+# print(construct_file_name(lat=36, lon=-82))
+# print(" == ")
+# print("USGS_NED_1_n36w082_IMG.tif\n")
+# print(construct_file_name(lat=-36, lon=82))
+# print(" == ")
+# print("USGS_NED_1_s36e082_IMG.tif\n")
+
+
+# print('1.2 testing')
+# im = load_trim_image(lat=38, lon=-84)
+# print(im.shape)
+# h, w = im.shape
+# print(im[h//2-5:h//2+5, w//2-5:w//2+5])
+# print(im[:10, :10])
+# plt.imshow(im[:32,:32])
+# plt.colorbar()
+# plt.show()
+
+# print('1.3 testing')
+# im = stitch_four(37, -83)
+# print(im.shape)
+# h, w = im.shape
+# print(im[h//2-5:h//2+5, w//2-5:w//2+5])
+# plt.imshow(im[h//2-16:h//2+16, w//2-16:w//2+16])
+# plt.colorbar()
+# plt.show()
+# plt.imshow(im)
+# plt.colorbar()
+# plt.show()
+
+# print("1.4 testing")
+# im = get_row(lat=38, lon_min=-84, num_tiles=1)
+# print(im.shape)
+# h, w = im.shape
+# print(im[h//2-5:h//2+5, w//2-5:w//2+5])
+# plt.imshow(im[h//2-16:h//2+16, w//2-16:w//2+16])
+# plt.colorbar()
+# plt.show()
+# plt.imshow(im)
+# plt.colorbar()
+# plt.show()
+
+# print("1.5 testing")
+# im = get_tile_grid(lat_max=37, lon_min=-83, num_lat=2, num_lon=2)
+# im.shape
+# h, w = im.shape
+# print(im[h//2-5:h//2+5, w//2-5:w//2+5])
+# plt.imshow(im[h//2-16:h//2+16, w//2-16:w//2+16])
+# plt.colorbar()
+# plt.show()
+# plt.imshow(im)
+# plt.colorbar()
+# plt.show()
+
+# print("1.6 testing")
+# print(get_northwest(lat=36.211389, lon=-81.668611))
+# print('==')
+# print("(37, -82)\n")
+
+# print('1.7 testing')
+# nw = (37.2, -82.7)
+# se = (36.6, -82.5)
+# im = get_tile_grid_decimal(nw, se)
+# print(im.shape)
+# nw = (37.2, -83.7)
+# se = (36.6, -81.5)
+# im = get_tile_grid_decimal(nw, se)
+# print(im.shape)
+# nw = (37.5, -82.5)
+# se = (36.5, -81.5)
+# im = get_tile_grid_decimal(nw, se)
+# print(im.shape)
+# h, w = im.shape
+# print(im[h//2-5:h//2+5, w//2-5:w//2+5])
+# plt.imshow(im[h//2-16:h//2+16, w//2-16:w//2+16])
+# plt.colorbar()
+# plt.show()
+# plt.imshow(im)
+# plt.colorbar()
+# plt.show()
+
+
+#2.1
+def dec_to_dms(dec):
+
+    is_positive = dec >= 0
+    dec = abs(dec)
+    minutes,seconds = divmod(dec*3600,60)
+    degrees,minutes = divmod(minutes,60)
+    degrees = degrees if is_positive else -degrees
+    
+    return (degrees,np.round(minutes),np.round(seconds))
+
+
+#2.2
+def seconds_to_index(seconds):
+
+    if (seconds == 0):
+        return 0
+    else:
+        return 3600-seconds
+
+#2.3
+def get_trim(northwest, southeast):
+    
+    t, l = northwest
+    b, r = southeast
+
+    tdeg, tmin, tsec = dec_to_dms(t)
+    tsec = tsec + (tmin * 60)
+    top = seconds_to_index(tsec)
+
+    ldeg, lmin, lsec = dec_to_dms(l)
+    lsec = lsec + (lmin * 60)
+    left = seconds_to_index(lsec)
+
+    bdeg, bmin, bsec = dec_to_dms(b)
+    bsec = bsec + (bmin * 60)
+    bottom = 3599 - seconds_to_index(bsec)
+
+    rdeg, rmin, rsec = dec_to_dms(r)
+    rsec = rsec + (rmin * 60)
+    right = 3599 - seconds_to_index(rsec)
+
+    return left, right, bottom, top
+
+#2.4
+def get_roi(center, n):
+
+    
+
+    return northwest, southeast
+
+
+#2.5
+#def crop(im, trim):
+
+#2.6
+#def get_extent(northwest, southeast):
+
+#2.7
+#def zoom(center, n)
+
+
+
+
+#--- Testing Section 2 ---#
+
+# print("Testing 2.1")
+# print(dec_to_dms(0.005555555525127431))
+# print("==")
+# print("(0.0, 0, 20)\n")
+
+
+# print("Testing 2.2")
+# print(seconds_to_index(0))
+# print("==")
+# print("0\n")
+
+
+# print("Testing 2.3")
+# nw = (36, -82)
+# se = (35+1/3600, -81-1/3600)
+# print(get_trim(nw, se))
+# print("==")
+# print("0, 0, 0, 0\n")
+# nw = (36-4/3600, -82+1/3600)
+# se = (35+4/3600, -81-3/3600)
+# print(get_trim(nw, se))
+# print("==")
+# print("1, 2, 3, 4\n")
+# nw = (38-4/3600, -84+1/3600)
+# se = (32+4/3600, -80-3/3600)
+# print(get_trim(nw, se))
+# print("==")
+# print("1, 2, 3, 4\n")
